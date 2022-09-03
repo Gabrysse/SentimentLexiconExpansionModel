8b8096439bed72fbcbbd2bc963092d1b5182dee3
Gabriele <gabrielerosi97@gmail.com>
2022-09-03 16:18:08 +0200

    Difference between checkpoint to find the word that have changed more

--- test.py
+++ test.py
@@ -6,19 +6,48 @@ from neural.net_softmax import NetSoftmax
 from dataset.Utilities import read_glove, getIMDBDF, getHotelReviewDF, getFakeNewsDF, getCoronaDF, getSpamDF
 
 
-def word_ranking(word_dict, N=10):
+def word_ranking(word_dict, N=10, diff=False):
     top_neg_words = dict(sorted(word_dict.items(), key=lambda item: item[1])[0:N])
     top_pos_words = dict(sorted(word_dict.items(), key=lambda item: item[1], reverse=True)[0:N])
+
+    if diff:
+        print(f"👍 TOP {N} WORDS CHANGED IN POSITIVE")
+        for elem in list(top_pos_words.items()):
+            print(elem)
+        print(f"👎 TOP {N} WORDS CHANGED IN NEGATIVE")
+        for elem in list(top_neg_words.items()):
+            print(elem)
+    else:
+        print(f"👍 TOP {N} POSITIVE WORDS")
+        for elem in list(top_pos_words.items()):
+            print(elem)
+        print(f"👎 TOP {N} NEGATIVE WORDS")
+        for elem in list(top_neg_words.items()):
+            print(elem)
 
     return top_pos_words, top_neg_words
 
 
+def word_difference(dict1, dict2):
+    difference = {}
+    for key in dict2.keys():
+        if key in dict1.keys():
+            if (dict2[key] > 0 and dict1[key] < 0) or (dict2[key] < 0 and dict1[key] > 0):
+                difference[key] = dict2[key] - dict1[key]
+
+    return difference
+
 def main(args):
     glove = read_glove()
 
-    loaded_checkpoint = torch.load(args.checkpoint)
-    model = NetSoftmax(loaded_checkpoint['scale_min'], loaded_checkpoint['scale_max'])
-    model.load_state_dict(loaded_checkpoint['model_state_dict'])
+    loaded_checkpoint = torch.load(args.checkpoint1)
+    model1 = NetSoftmax(loaded_checkpoint['scale_min'], loaded_checkpoint['scale_max'])
+    model1.load_state_dict(loaded_checkpoint['model_state_dict'])
+
+    if args.checkpoint2 is not None:
+        loaded_checkpoint = torch.load(args.checkpoint2)
+        model2 = NetSoftmax(loaded_checkpoint['scale_min'], loaded_checkpoint['scale_max'])
+        model2.load_state_dict(loaded_checkpoint['model_state_dict'])
 
     for dataset in args.unsup_dataset.split(" "):
         df = None
@@ -39,21 +68,24 @@ def main(args):
             df = getSpamDF()
 
         if df is not None:
-            accuracy, cache = unsupervised_review_sentiment(df, model, glove)
-            print(f"🏆 {name} accuracy {accuracy}")
+            accuracy1, cache1 = unsupervised_review_sentiment(df, model1, glove)
+            print(f"🏆 {name} accuracy {accuracy1}")
             if args.word_ranking:
-                top_pos_words, top_neg_words = word_ranking(cache)
-                print("👍 POSITIVE WORDS RANKING")
-                for elem in list(top_pos_words.items()):
-                    print(elem)
-                print("👎 NEGATIVE WORDS RANKING")
-                for elem in list(top_neg_words.items()):
-                    print(elem)
+                top_pos_words, top_neg_words = word_ranking(cache1)
+
+            if args.checkpoint2 is not None:
+                accuracy2, cache2 = unsupervised_review_sentiment(df, model2, glove)
+                print(f"🏆 {name} accuracy {accuracy2}")
+                if args.word_ranking:
+                    top_pos_words, top_neg_words = word_ranking(cache2)
+                    difference = word_difference(cache1, cache2)
+                    word_ranking(difference)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--checkpoint', type=str, default='net2.pth', help='Checkpoint path')
+    parser.add_argument('--checkpoint1', type=str, default='net2.pth', help='Checkpoint path')
+    parser.add_argument('--checkpoint2', type=str, default=None, help='Checkpoint path')
     parser.add_argument('--unsup_dataset', type=str, help='Dataset used for unsupervised sentiment score. '
                                                           'Allowed values: imdb hotel fake_news covid_tweet spam')
     parser.add_argument('--word_ranking', action="store_true", help="Use this if you want to get the ranking of "
